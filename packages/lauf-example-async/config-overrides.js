130577603091a15cf85dc4b099d9d1361a7757c3
Cefn Hoile <github.com@cefn.com>
2021-02-16 22:23:38 +0000

    Make config-overrides.js safer

--- packages/lauf-example-async/config-overrides.js
+++ packages/lauf-example-async/config-overrides.js
@@ -2,17 +2,23 @@
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = (config) => {
-  // Remove the ModuleScopePlugin which throws when we try
-  // to import something outside of src/.
-  config.resolve.plugins.pop();
-
-  // Resolve the path aliases.
-  config.resolve.plugins.push(new TsconfigPathsPlugin());
+  config.resolve.plugins = [
+    // remove plugin that throws  when importing anything outside of src/
+    ...config.resolve.plugins.filter(
+      (plugin) => plugin.constructor.name !== "ModuleScopePlugin"
+    ),
+    // add plugin that resolves path aliases
+    new TsconfigPathsPlugin(),
+  ];
 
   // Let Babel compile outside of src/.
   const oneOfRule = config.module.rules.find((rule) => rule.oneOf);
-  const tsRule = oneOfRule.oneOf.find((rule) =>
-    rule.test.toString().includes("ts|tsx")
+  const tsRule = oneOfRule.oneOf.find(
+    (rule) =>
+      rule.include &&
+      rule.include.endsWith("/src") &&
+      rule.loader.includes("babel-loader") &&
+      rule.test.toString().includes("ts|tsx")
   );
   tsRule.include = undefined;
   tsRule.exclude = /node_modules/;
