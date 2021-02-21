0068941c98c50b259543b945b8fb8a51c051fe3f
Cefn Hoile <github.com@cefn.com>
2021-02-21 22:54:57 +0000

    Commented, reordered

--- packages/lauf-runner/src/types.ts
+++ packages/lauf-runner/src/types.ts
@@ -2,12 +2,9 @@ export interface Action<Reaction> {
   act: () => Reaction | Promise<Reaction>;
 }
 
-export interface ActionClass<Params extends any[], Reaction> {
-  new (...params: Params): Action<Reaction>;
-}
-
-/** Generator with a next() that accepts the awaited result of
- * its own yielded actions until an Ending is returned.
+/** A Performance is a Generator with a next() that yields actions and
+ * accepts their resolutions in return, until an Ending
+ * is returned.
  */
 export type Performance<Ending = void, Reaction = any> = Generator<
   Action<Reaction>,
@@ -15,9 +12,14 @@ export type Performance<Ending = void, Reaction = any> = Generator<
   Reaction
 >;
 
-/** A Script is step-by-step instructions for a performance. */
+/** Script contains step-by-step instructions for a Performance. */
 export type Script<
   Args extends any[] = any[],
   Ending = void,
   Reaction = any
 > = (...args: Args) => Performance<Ending, Reaction>;
+
+/** Utility interface for classes that implement Action */
+export interface ActionClass<Params extends any[], Reaction> {
+  new (...params: Params): Action<Reaction>;
+}
