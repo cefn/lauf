45b0911fdafa6651cc84bdb67c437e84309cc10c
Cefn Hoile <github.com@cefn.com>
2021-02-25 20:38:35 +0000

    Draft changes to perform

--- packages/lauf-runner-trial/src/perform.ts
+++ packages/lauf-runner-trial/src/perform.ts
@@ -1,15 +1,57 @@
 import type { Action, Performance } from "@lauf/lauf-runner";
 import type { Performer } from "./types";
 
+type GeneratorFactory<Params extends any[], T, TReturn, TNext> = (
+  ...params: Params
+) => Generator<T, TReturn, TNext>;
+
+type AsyncGeneratorFactory<Params extends any[], T, TReturn, TNext> = (
+  ...params: Params
+) => AsyncGenerator<T, TReturn, TNext>;
+
+export function initialiseGenerator<Params extends any[], T, TReturn, TNext>(
+  generatorFactory: GeneratorFactory<Params, T, TReturn, TNext>,
+  ...params: Params
+): [Generator<T, TReturn, TNext>, T] {
+  const generator = generatorFactory(...params);
+  const generatorResult = generator.next();
+  if (generatorResult.done) {
+    throw new Error(`Generator from ${generatorFactory} returned immediately`);
+  } else {
+    return [generator, generatorResult.value];
+  }
+}
+
+export async function initialiseAsyncGenerator<
+  Params extends any[],
+  T,
+  TReturn,
+  TNext
+>(
+  asyncGeneratorFactory: AsyncGeneratorFactory<Params, T, TReturn, TNext>,
+  ...params: Params
+): Promise<[AsyncGenerator<T, TReturn, TNext>, T]> {
+  const generator = asyncGeneratorFactory(...params);
+  const generatorResult = await generator.next();
+  if (generatorResult.done) {
+    throw new Error(
+      `Generator from ${asyncGeneratorFactory} returned immediately`
+    );
+  } else {
+    return [generator, generatorResult.value];
+  }
+}
+
 export async function perform<Ending, Reaction>(
+  trigger: Reaction,
   performance: Performance<Ending, Reaction>,
   performer: Performer<Reaction, any> = actor
 ): Promise<Ending> {
-  const routine = performer(); //todo this should be called performance
+  let [routine, reactionResult] = await initialiseAsyncGenerator(performer);
+  //todo 'routine' should be called performance
   try {
     //prime both generators up to first 'yield'
-    let actionResult = performance.next();
-    let reactionResult = await routine.next();
+    let actionResult = performance.next(trigger);
     while (!actionResult.done) {
       reactionResult = await routine.next(actionResult.value);
       actionResult = performance.next(reactionResult.value);
