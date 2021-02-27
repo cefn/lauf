6f7f4b2f5d2662eb1831d2cb5638a344bd83dbb4
Cefn Hoile <github.com@cefn.com>
2021-02-27 02:29:55 +0000

    Marginally improved test

--- packages/lauf-example-async/test/plans.test.ts
+++ packages/lauf-example-async/test/plans.test.ts
@@ -11,25 +11,40 @@ import {
   FetchSubreddit,
   AppState,
   initialAppState,
+  fetchPlan,
 } from "../src/plans";
 
 describe("Test domain logic", () => {
   describe("Main plan", () => {
-    test("Posts for initially-focused subreddit are retrieved and stored", async () => {
+    test("Initially-focused subreddit: Posts retrieved and stored", async () => {
+      const { focus } = initialAppState;
       const store = new BasicStore<AppState>(initialAppState);
-      const initialFocus = initialAppState.focus;
 
       async function* testPerformer(action: Action<any>) {
-        yield* actUntilActionMatches(action, new FetchSubreddit(initialFocus));
+        yield* actUntilActionMatches(action, new FetchSubreddit(focus));
         const posts = [{ title: "Hey" }, { title: "Yo" }];
-        yield* actUntilReactionFulfils(yield posts, (reaction: any) => {
-          const maybePosts = store.getValue().caches?.[initialFocus]?.posts;
-          return isDeepStrictEqual(posts, maybePosts);
+        yield* actUntilReactionFulfils(yield posts, () => {
+          const storedPosts = store.getValue().caches?.[focus]?.posts;
+          return isDeepStrictEqual(posts, storedPosts);
         });
       }
 
-      const mainSequence = mainPlan(store);
-      await directSequence(testPerformer, mainSequence);
+      await directSequence(testPerformer, mainPlan(store));
+    });
+
+    //TODO test that a change of focus causes a fetch request.
+  });
+
+  describe("Fetch plan", () => {
+    test("", async () => {
+      const subredditName = "frontend";
+      const store = new BasicStore<AppState>(initialAppState);
+
+      async function* testPerformer(action: Action<any>) {
+        //TODO write test
+      }
+
+      await directSequence(testPerformer, fetchPlan(store, subredditName));
     });
   });
 });
