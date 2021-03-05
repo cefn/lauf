c9a9322bd4109531031a74e9150866c30b299c15
Cefn Hoile <github.com@cefn.com>
2021-03-05 02:49:32 +0000

    Introduces a draft snake written in nextjs (#19)
    Draft snake app

---
+++ apps/nextjs-snake/pages/index.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Head from "next/head";
+
+import { useSelected } from "@lauf/lauf-store-react";
+import {
+  gridDigits,
+  gameStore,
+  selectSegments,
+  DirectionName,
+  steer,
+} from "../plan";
+
+const directionMap: Record<string, DirectionName> = {
+  ArrowLeft: "left",
+  ArrowUp: "up",
+  ArrowRight: "right",
+  ArrowDown: "down",
+} as const;
+
+if (process.browser) {
+  document.addEventListener("keydown", ({ code }) => {
+    const direction = directionMap[code];
+    if (direction) {
+      steer(direction);
+    }
+  });
+}
+
+export default function index() {
+  const segments = useSelected(gameStore, selectSegments);
+  return (
+    <>
+      <Head>
+        <title>Snake</title>
+      </Head>
+      <style global jsx>{`
+        html,
+        body,
+        div#__next {
+        }
+      `}</style>
+      <div style={{ display: "inline-block" }}>
+        {gridDigits.map((y) => (
+          <pre
+            style={{
+              border: "0px",
+              padding: "0px",
+              margin: "0px",
+              fontSize: "3em",
+              backgroundColor: "red",
+            }}
+          >
+            {gridDigits
+              .map((x) => {
+                let line = "";
+                for (const segment of segments) {
+                  const [segX, segY] = segment.pos;
+                  if (segX === x && segY === y) {
+                    return "O";
+                  }
+                }
+                return " ";
+              })
+              .join("")}
+          </pre>
+        ))}
+      </div>
+    </>
+  );
+}
---
+++ apps/nextjs-snake/util.ts
@@ -0,0 +1,17 @@
+import { Immutable } from "@lauf/lauf-store";
+import { Vector, gridMax, gridSpan } from "./plan";
+
+export function sum(vecA: Immutable<Vector>, vecB: Immutable<Vector>): Vector {
+  return [vecA[0] + vecB[0], vecA[1] + vecB[1]];
+}
+
+export function wrap(vec: Vector): Vector {
+  return vec.map((dim) => {
+    const edgeOffset = dim + gridMax;
+    if (edgeOffset >= 0 && edgeOffset < gridSpan) {
+      return dim;
+    } else {
+      return (edgeOffset % gridSpan) - gridMax;
+    }
+  }) as Vector;
+}
