0c25229769cb65dc5db519396fbb2ea0976b5ae0
Cefn Hoile <github.com@cefn.com>
2021-03-11 12:05:34 +0000

    Prevent coverage backsliding

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,13 @@ module.exports = {
     "<rootDir>/modules/*/jest.config.js",
     // "<rootDir>/docs/tutorialcode/jest.config.js",
   ],
+  coverageDirectory: "<rootDir>/coverage/",
+  coverageThreshold: {
+    global: {
+      statements: 83.81,
+      branches: 61.86,
+      functions: 78.42,
+      lines: 83.96,
+    },
+  },
 };
