4015ecb5209980aec9696189133d789adea55c49
Cefn Hoile <github.com@cefn.com>
2021-04-01 11:53:27 +0100

    Retire Immutable in favour of DeepReadonly

--- modules/lauf-store/src/types/immutable.ts
+++ modules/lauf-store/src/types/immutable.ts
@@ -1,17 +1,8 @@
 import type { Draft } from "immer";
+import type { DeepReadonly } from "ts-essentials";
 
-export type ImmutableObject<T> = Readonly<
-  {
-    [K in keyof T]: Immutable<T[K]>;
-  }
->;
+type Immutable<T> = DeepReadonly<T>;
 
-export type Immutable<T> = T extends any[] | object
-  ? ImmutableObject<T>
-  : T extends string | number | boolean | null
-  ? Readonly<T>
-  : never;
+type Editor<T> = (draft: Draft<Immutable<T>>) => void;
 
-export type Editor<T> = (draft: Draft<Immutable<T>>) => void;
-
-export type { Draft };
+export type { Immutable, Draft, Editor };
