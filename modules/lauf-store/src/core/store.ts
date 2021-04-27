09e5e0da503dfb77812d5d3215e70b1c1077b0f1
Cefn Hoile <github.com@cefn.com>
2021-04-27 22:03:57 +0100

    Eliminated partitioner compile errors

--- modules/lauf-store/src/core/store.ts
+++ modules/lauf-store/src/core/store.ts
@@ -1,9 +1,9 @@
-import type { Selector, Store } from "../types";
+import type { ObjectParent, Partitioner, Selector, Store } from "../types";
 import type { Editor, Immutable } from "../types/immutable";
 import { BasicWatchable, BasicWatchableValue } from "./watchable";
 import { castDraft, Draft, produce } from "immer";
 
-export class BasicStore<State extends object>
+export class BasicStore<State>
   extends BasicWatchableValue<Immutable<State>>
   implements Store<State> {
   edit = (editor: Editor<State>) => {
@@ -16,32 +16,32 @@ export class BasicStore<State extends object>
   select = <Selected>(selector: Selector<State, Selected>) => {
     return selector(this.read());
   };
-  partition = (key: keyof State) => new BasicStorePartition(this, key);
+  partition: Partitioner<State> = <Key extends keyof State>(key: Key) =>
+    new BasicStorePartition(this, key);
 }
 
 export class BasicStorePartition<
-    State extends object,
-    Key extends keyof State,
-    SubState extends State[Key] & object
+    State extends ObjectParent,
+    Key extends keyof State
   >
-  extends BasicWatchable<Immutable<SubState>>
-  implements Store<SubState> {
+  extends BasicWatchable<Immutable<State[Key]>>
+  implements Store<State[Key]> {
   constructor(readonly store: Store<State>, readonly key: keyof State) {
     super();
     this.track();
   }
   private track = () => {
-    let lastSubState: SubState | undefined;
+    let lastSubState: State[Key] | undefined;
     this.store.watch((state) => {
       const subState = state[this.key];
       if (Object.is(subState, lastSubState)) {
         return;
       }
-      this.notify(subState as Immutable<SubState>);
+      this.notify((subState as unknown) as Immutable<State[Key]>);
     });
   };
   read = () => {
-    return (this.store.read()[this.key] as unknown) as Immutable<SubState>;
+    return (this.store.read()[this.key] as unknown) as Immutable<State[Key]>;
   };
   write = (state: Immutable<State[Key]>) => {
     this.store.edit((draft: Draft<Immutable<State>>) => {
@@ -49,15 +49,17 @@ export class BasicStorePartition<
     });
     return this.read();
   };
-  edit = (editor: Editor<SubState>) => {
+  edit = (editor: Editor<State[Key]>) => {
     this.store.edit((draft: Draft<Immutable<State>>) => {
       const substate = draft[this.key as keyof Draft<Immutable<State>>];
-      editor(substate as Draft<Immutable<SubState>>);
+      editor(substate as Draft<Immutable<State[Key]>>);
     });
     return this.read();
   };
-  select = <Selected>(selector: Selector<SubState, Selected>) => {
+  select = <Selected>(selector: Selector<State[Key], Selected>) => {
     return selector(this.read());
   };
-  partition = (key: keyof SubState) => new BasicStorePartition(this, key);
+  partition: Partitioner<State[Key]> = <SubKey extends keyof State[Key]>(
+    subKey: SubKey
+  ) => new BasicStorePartition(this, subKey);
 }
--- modules/lauf-store/src/types/store.ts
+++ modules/lauf-store/src/types/store.ts
@@ -6,9 +6,19 @@ export interface Store<State> extends WatchableValue<Immutable<State>> {
   select: <Selected>(
     selector: Selector<State, Selected>
   ) => Immutable<Selected>;
-  // partition: (key: keyof State) => Store<State[typeof key]>;
+  partition: Partitioner<State>;
 }
 
 export type Selector<State, Selected> = (
   state: Immutable<State>
 ) => Immutable<Selected>;
+
+export type Partitioner<State> = <Key extends keyof State>(
+  key: Key
+) => Store<State[Key]>;
+
+export type FilterValues<T, V> = {
+  [K in keyof T]: T[K] extends V ? T[K] : never;
+};
+
+export type ObjectParent = FilterValues<{}, object>;
