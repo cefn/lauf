4aaa0593665b65428ecec11b1e7b21fbf0b10ce4
Cefn Hoile <github.com@cefn.com>
2021-05-04 00:14:17 +0100

    Use tracker's performer to intercept,record events

--- modules/lauf-runner-track/src/index.ts
+++ modules/lauf-runner-track/src/index.ts
@@ -6,25 +6,75 @@ import {
   directPlan,
   Performer,
 } from "@lauf/lauf-runner";
-import { Store } from "@lauf/lauf-store";
+import { Immutable, Store } from "@lauf/lauf-store";
+
+export class Event {
+  readonly time: number;
+  constructor(readonly ordinal: number) {
+    this.time = new Date().getTime();
+  }
+}
+
+export class ForkEvent extends Event {
+  constructor(readonly forkHandle: ForkHandle<any, any, any>, ordinal: number) {
+    super(ordinal);
+  }
+}
+
+export class ActionEvent<Reaction> extends ForkEvent {
+  constructor(
+    readonly action: Action<Reaction>,
+    forkHandle: ForkHandle<any, any, any>,
+    ordinal: number
+  ) {
+    super(forkHandle, ordinal);
+  }
+}
+
+export class ReactionEvent<Reaction> extends ForkEvent {
+  constructor(
+    readonly reaction: Reaction,
+    forkHandle: ForkHandle<any, any, any>,
+    ordinal: number
+  ) {
+    super(forkHandle, ordinal);
+  }
+}
+
+export class StoreEvent<State> extends Event {
+  readonly state: Immutable<State>;
+  constructor(readonly store: Store<State>, ordinal: number) {
+    super(ordinal);
+    this.state = store.read();
+  }
+}
 
 type ForkId = string;
 
-interface ForkHandle {
+interface ForkHandle<Args extends any[], Ending, Reaction> {
   id: ForkId;
   performer: Performer;
+  plan: ActionPlan<Args, Ending, Reaction>;
+  args: Args;
+  ending?: Ending;
 }
 
 export class Tracker<State> {
   protected nextForkOrdinal = 0;
   protected nextEventOrdinal = 0;
-  protected forkHandles: Record<ForkId, ForkHandle> = {};
-  protected initialState: State;
+  forkHandles: Immutable<Record<ForkId, ForkHandle<any, any, any>>> = {};
+  events: Immutable<Event[]> = [];
   constructor(
     readonly store: Store<State>,
     readonly performer: Performer = ACTOR
   ) {
-    this.initialState = store.read();
+    const recordState = () =>
+      (this.events = [
+        ...this.events,
+        new StoreEvent(store, this.assignEventId()),
+      ]);
+    recordState(); //record initial state
+    store.watch(recordState); //track future events
   }
 
   assignForkId() {
@@ -37,9 +87,10 @@ export class Tracker<State> {
 
   protected createForkHandle<Args extends any[], Ending, Reaction>(
     plan: ActionPlan<Args, Ending, Reaction>,
+    args: Args,
     performer: Performer,
-    parent?: ForkHandle
-  ): ForkHandle {
+    parent?: ForkHandle<any, any, any>
+  ): ForkHandle<Args, Ending, Reaction> {
     //construct id
     let id = parent ? `${parent.id}-` : "";
     id += this.assignForkId();
@@ -48,12 +99,15 @@ export class Tracker<State> {
     //construct handle (including child fork interception)
     const forkHandle = {
       id,
+      plan,
+      args,
       performer: async (action: Action<any>) => {
         //intercept child fork
         if (action instanceof BackgroundPlan) {
           //create child handle, constructing forked performer
           const childForkHandle = this.createForkHandle(
             action.plan,
+            action.args,
             action.performer,
             forkHandle
           );
@@ -64,11 +118,22 @@ export class Tracker<State> {
             childForkHandle.performer
           );
         }
+        this.events = [
+          ...this.events,
+          new ActionEvent(action, forkHandle, this.assignEventId()),
+        ];
         const reaction = await performer(action);
+        this.events = [
+          ...this.events,
+          new ReactionEvent(reaction, forkHandle, this.assignEventId()),
+        ];
         return reaction;
       },
     };
-    this.forkHandles[forkHandle.id] = forkHandle;
+    this.forkHandles = {
+      ...this.forkHandles,
+      [forkHandle.id]: forkHandle,
+    };
     return forkHandle;
   }
 
@@ -76,11 +141,17 @@ export class Tracker<State> {
     plan: ActionPlan<Args, Ending, Reaction>,
     ...args: Args
   ): Promise<Ending> {
+    //create root forkHandle
     const forkHandle = this.createForkHandle(
       plan,
+      args,
       this.performer
-      /* root plan has no parent handle */
+      /* root has no parent */
     );
-    return await directPlan(plan, args, forkHandle.performer);
+    return await directPlan(
+      forkHandle.plan,
+      forkHandle.args,
+      forkHandle.performer
+    );
   }
 }
--- modules/lauf-runner-track/test/index.test.ts
+++ modules/lauf-runner-track/test/index.test.ts
@@ -1,25 +1,99 @@
-import { ActionSequence, call } from "@lauf/lauf-runner";
-import { BasicStore, Store } from "@lauf/lauf-store/src";
-import { Tracker } from "../src";
-
-interface State {
-  counter: number;
-}
-
-function* countPlan(store: Store<State>): ActionSequence<void, any> {
-  for (const i of [3, 4, 5]) {
-    yield* call(store.edit, (state) => {
-      state.counter = i;
-    });
-  }
-}
+import assert from "assert";
+import {
+  ActionSequence,
+  ACTOR,
+  backgroundPlan,
+  Call,
+  call,
+  wait,
+} from "@lauf/lauf-runner";
+import { BasicStore, Store } from "@lauf/lauf-store";
+import { Tracker, ActionEvent, ReactionEvent, StoreEvent } from "../src";
 
 describe("Track actions and reactions from running sequences", () => {
-  test("Actions and Reactions are performed as usual", async () => {
+  /** Introduce minimal Store state and plans for testing */
+
+  interface State {
+    counter: number;
+  }
+
+  function* countPlan(store: Store<State>): ActionSequence<void, any> {
+    for (const i of [3, 4, 5]) {
+      yield* call(store.edit, (state) => {
+        state.counter = i;
+      });
+    }
+  }
+
+  test("Actions and Reactions are performed as usual : single plan", async () => {
+    //Configure, track and run plan
+    const store = new BasicStore<State>({ counter: 0 });
+    const tracker = new Tracker(store, ACTOR);
+    await tracker.performPlan(countPlan, store);
+    //Check end result is as normal
+    expect(store.read()).toEqual({ counter: 5 });
+  });
+
+  test("Actions and Reactions are performed as usual : forked plan", async () => {
+    function* forkingCountPlan(store: Store<State>): ActionSequence<void, any> {
+      const [countPromise] = yield* backgroundPlan(countPlan, store);
+      yield* wait(countPromise);
+    }
+
+    //Configure, track and run plan
+    const store = new BasicStore<State>({ counter: 0 });
+    const tracker = new Tracker(store);
+    await tracker.performPlan(forkingCountPlan, store);
+    //Check end result is as normal
+    expect(store.read()).toEqual({ counter: 5 });
+  });
+
+  test("Actions, Reactions, State changes are recorded in tracker", async () => {
+    //Configure, track and run plan
     const store = new BasicStore<State>({ counter: 0 });
     const tracker = new Tracker(store);
     await tracker.performPlan(countPlan, store);
-    expect(store.read()).toEqual({ counter: 5 });
+
+    //Check basic number of events is as expected
+    expect(tracker.events.length).toBe(10);
+
+    let pos = 0,
+      event;
+
+    /** First event records initial state */
+    event = tracker.events[pos++];
+    assert(event instanceof StoreEvent);
+    expect(event.store).toBe(store);
+    expect(event.state).toEqual({
+      counter: 0,
+    });
+
+    //Each edit triggers a sequence of events:
+    for (const newValue of [3, 4, 5]) {
+      /** First the edit is instructed*/
+      event = tracker.events[pos++];
+      assert(event instanceof ActionEvent);
+      assert(event.action instanceof Call);
+      expect(event.action.fn).toBe(store.edit);
+
+      /** On change the edited state is notified  */
+      event = tracker.events[pos++];
+      assert(event instanceof StoreEvent);
+      expect(event.store).toBe(store);
+      expect(event.state).toEqual({
+        counter: newValue,
+      });
+
+      /** Finally the edit instruction returns */
+      event = tracker.events[pos++];
+      assert(event instanceof ReactionEvent);
+      expect(event.reaction).toEqual({
+        counter: newValue,
+      });
+    }
+
+    //Events end
+    expect(pos).toBe(tracker.events.length);
   });
 
   // test("", () => {});
