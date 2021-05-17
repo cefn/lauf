a4e86de0ff4acf670f6ca02a8b0880e3ffe2e421
Cefn Hoile <github.com@cefn.com>
2021-05-17 01:41:24 +0100

    Layout as split-screen

--- apps/nextjs-snake/pages/index.tsx
+++ apps/nextjs-snake/pages/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import cssType from "csstype";
 import { mainPlan } from "../src/game";
 import { AppModel, Direction, GameState, INITIAL_STATE } from "../src/domain";
 import { Game } from "../src/view";
@@ -7,6 +8,15 @@ import { Tracker } from "@lauf/lauf-runner-track";
 import { PlanView } from "@lauf/lauf-runner-planview";
 import { BasicStore } from "@lauf/lauf-store/src";
 import { BasicMessageQueue } from "@lauf/lauf-queue/src";
+
+const splitStyle: cssType.Properties = {
+  height: "100%",
+  width: "50%",
+  position: "fixed",
+  overflowX: "hidden",
+  zIndex: 1,
+  top: 0,
+};
 
 export default function render() {
   const [appModel, setAppModel] = useState<AppModel>();
@@ -28,8 +38,12 @@ export default function render() {
 
   return appModel && tracker ? (
     <>
-      <Game {...appModel} />
-      <PlanView tracker={tracker} />
+      <div style={{ ...splitStyle, left: 0 }}>
+        <Game {...appModel} />
+      </div>
+      <div style={{ ...splitStyle, right: 0 }}>
+        <PlanView tracker={tracker} />
+      </div>
     </>
   ) : (
     <p>Loading...</p>
