44031cfded7273364719898096b57d66b6fb7624
Cefn Hoile <github.com@cefn.com>
2021-05-24 01:15:04 +0100

    Prepare all modules for stable npm publish.

---
+++ modules/lauf-store/README.md
@@ -0,0 +1,12 @@
+<img src="https://github.com/cefn/lauf/raw/main/vector/logo.png" alt="Logo - Image of Runner" align="left"><br></br>
+
+# Lauf Store
+
+<sub><sup>Logo - Diego Naive, Noun Project.</sup></sub>
+<br></br>
+
+`@lauf/lauf-store` provides a minimal reactive state-management solution, a simple substitute for Flux/Redux based on [Immer](https://immerjs.github.io/immer/).
+
+It is incredibly lightweight and suitable for adoption with almost any server-side or client-side framework in Typescript or Javascript.
+
+A React binding is provided by the [@lauf/lauf-store-react](https://github.com/cefn/lauf/tree/main/modules/lauf-store-react) package
--- validate-packages.ts
+++ validate-packages.ts
@@ -1,4 +1,12 @@
 #!/usr/bin/env -S npx ts-node
+/**
+ * An example of running this script surgically to fix just one property...
+ * npx ./validate-packages.ts
+ * ...will report on all errors and warnings...
+ * npx ./validate-packages.ts --filterPropertyPaths=publishConfig --strategy=dryRun
+ * ...to report errors for a specific property before force-fixing, then use the fixErrors strategy
+ * npx ./validate-packages.ts --filterPropertyPaths=publishConfig --strategy=fixErrors
+ */
 import assert from "assert";
 import fs from "fs";
 import glob from "glob";
@@ -11,6 +19,34 @@ import chalk from "chalk";
 
 const RULES: ReadonlyArray<PackageJsonRule> = [
   {
+    path: "homepage",
+    expected: "https://github.com/cefn/lauf#readme",
+    status: "error",
+  },
+  {
+    path: "author",
+    expected: "Cefn Hoile <github.com@cefn.com> (https://cefn.com)",
+    status: "error",
+  },
+  {
+    path: "repository",
+    expected: "github:cefn/lauf",
+    status: "error",
+  },
+  {
+    path: "bugs",
+    expected: {
+      url: "https://github.com/cefn/lauf/issues",
+      email: "lauf@cefn.com",
+    },
+    status: "error",
+  },
+  {
+    path: "devDependencies.typescript",
+    expected: "^4.2.2",
+    status: "error",
+  },
+  {
     path: "scripts.test",
     expected: "jest",
     status: "warning",
@@ -21,24 +57,81 @@ const RULES: ReadonlyArray<PackageJsonRule> = [
     status: "error",
   },
   {
-    path: "devDependencies.typescript",
-    expected: "^4.2.2",
+    path: "scripts.prepare",
+    expected: "yarn run test && yarn run build",
+    packagePaths: "modules/**",
+    status: "error",
+  },
+  {
+    path: "license",
+    expected: "MIT",
+    packagePaths: "modules/**",
+    status: "error",
+  },
+  {
+    path: "scripts.beta",
+    expected: undefined,
+    packagePaths: "modules/**",
+    status: "error",
+  },
+  {
+    path: "scripts.build",
+    expected: "tsc --build ./tsconfig.build.json",
+    packagePaths: "modules/**",
+    status: "error",
+  },
+  {
+    path: "main",
+    expected: "dist/index.js",
+    packagePaths: "modules/**",
+    status: "error",
+  },
+  {
+    path: "files",
+    expected: ["README.md", "dist"],
+    packagePaths: "modules/**",
+    status: "error",
+  },
+  {
+    path: "private",
+    expected: true,
+    packagePaths: "apps/**",
+    status: "error",
+  },
+  {
+    path: "private",
+    expected: true,
+    packagePaths: "modules/draft/*/package.json",
+    status: "error",
+  },
+  {
+    path: "private",
+    expected: undefined,
+    packagePaths: "modules/*/package.json",
+    status: "error",
+  },
+  {
+    path: "publishConfig",
+    expected: {
+      access: "public",
+    },
+    packagePaths: "modules/*/package.json",
     status: "error",
   },
 ] as const;
 
-const { strategy, filterPackages, filterPaths } = yargs
+const { strategy, filterPackagePaths, filterPropertyPaths } = yargs
   .option("strategy", {
     description: "Select alignment strategy",
     type: "string",
     default: "dryRun",
     choices: ["dryRun", "fixErrors", "fixWarnings"],
   })
-  .option("filterPackages", {
-    description: "Filter pattern for package names",
+  .option("filterPackagePaths", {
+    description: "Filter pattern for package paths",
     type: "string",
   })
-  .option("filterPaths", {
+  .option("filterPropertyPaths", {
     description: "Filter pattern for property names",
     type: "string",
   })
@@ -52,26 +145,32 @@ const STATUSES = [
 
 type Status = typeof STATUSES[number];
 type Rule = typeof RULES[number];
+type Expected = true | string | object | RegExp | undefined;
 
 interface PackageJsonRule {
   /** The path to get/set within package.json (lodash) */
   path: string;
   /** The value or pattern expected at that path */
-  expected: string | RegExp;
+  expected: Expected;
+  /** A minimatch filter limiting this rule to certain packages */
+  packagePaths?: string;
+  /** Whether this should count as a warning, error */
   status: Status;
 }
 
-const packageJsonPaths = glob.sync("**/package.json", {
-  ignore: "**/node_modules/**/package.json",
-});
+const packageJsonPaths = glob
+  .sync("**/package.json", {
+    ignore: "**/node_modules/**/package.json",
+  })
+  .sort();
 
 let failed = false;
 
 for (const packageJsonPath of packageJsonPaths) {
   const packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString());
 
-  if (filterPackages) {
-    if (!minimatch(packageJson.name, filterPackages)) {
+  if (filterPackagePaths) {
+    if (!minimatch(packageJsonPath, filterPackagePaths)) {
       continue;
     }
   }
@@ -85,15 +184,21 @@ for (const packageJsonPath of packageJsonPaths) {
 
   //traverse package json tree, checking and (optionally) fixing
   type Violation = {
-    actual: string;
-    expected: string | RegExp;
+    actual: any;
+    expected: Expected;
     fixed: boolean;
   };
   const found: Record<Rule["path"], Violation> = {};
   let rewritePackageJson = false;
-  for (const { path, expected, status } of RULES) {
-    if (filterPaths) {
-      if (!minimatch(path, filterPaths)) {
+  for (const { path, expected, status, packagePaths } of RULES) {
+    if (filterPropertyPaths) {
+      if (!minimatch(path, filterPropertyPaths)) {
+        continue;
+      }
+    }
+
+    if (packagePaths) {
+      if (!minimatch(packageJsonPath, packagePaths)) {
         continue;
       }
     }
@@ -112,7 +217,9 @@ for (const packageJsonPath of packageJsonPaths) {
       }
       const message = `${violationColor(
         status.toUpperCase()
-      )} ${path} was '${chalk.red(actual)}' not '${chalk.green(expected)}'`;
+      )} ${path} was '${chalk.red(actual)}' not '${chalk.green(
+        JSON.stringify(expected)
+      )}'`;
       //check strategy, possibly skip fix depending on rule status
       if (
         (status === "error" && ["dryRun"].includes(strategy)) ||
@@ -137,12 +244,17 @@ for (const packageJsonPath of packageJsonPaths) {
       console.log(
         `${chalk.yellow(path)} ${chalk.red(actual)}${chalk.yellow(
           " not "
-        )}${chalk.green(expected)} (${
+        )}${chalk.green(JSON.stringify(expected))} (${
           fixed ? "FIXED" : `NOT FIXED (${strategy})`
         })`
       );
     }
   }
+
+  // const packageDir = dirname(packageJsonPath);
+  // const tsconfigBuildPath = `${packageDir}/tsconfig.build.json`;
+  // const tsconfigBuild = JSON.parse(fs.readFileSync(packageJsonPath).toString());
+  //Implement this using a `skel` folder instead
 
   if (rewritePackageJson) {
     fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, "  "));
