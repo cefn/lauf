c90624cfa63cca5b54e678921f4387fdd69b3101
Cefn Hoile <github.com@cefn.com>
2021-06-14 13:33:58 +0100

    Clarify new editor contract

--- modules/lauf-store/src/types/immutable.ts
+++ modules/lauf-store/src/types/immutable.ts
@@ -30,14 +30,22 @@ type CastDraft = typeof castDraft;
 /**  A function to perform edits on a [[RootState]]. The editor is passed a
  * `draft` - a mutable proxy of the Store's current `Immutable` `RootState`.
  *
- * Changes made to the mutable `draft` proxy within the editor are tracked. When
- * it returns, {@link https://immerjs.github.io/immer/ | Immer} efficiently
- * composes a new [[Immutable]] state to reflect the drafted changes, leaving
- * the old state intact. The new state is passed to [[Store.write]].
+ * You can make changes to the mutable `draft` proxy within the editor using any
+ * javascript syntax. When it returns,
+ * {@link https://immerjs.github.io/immer/ | Immer} efficiently composes a new
+ * [[Immutable]] state to reflect your drafted changes, leaving the old state
+ * intact. The new state is passed to [[Store.write]].
  *
- * Note: Immer's `castDraft` enables parts of the last `Immutable` state to be
- * assigned to the new mutable `draft` state for convenience. However you must
- * avoid actually mutating any part of the last state.
+ * The editor is equivalent to Immer's producer except returning a value doesn't
+ * replace the [[RootState]]. To replace the state call [[Store.write]] instead
+ * of using an editor. This eliminates Immer's runtime errors when you **draft**
+ * changes as well as returning a value, (easily done by accident in simple
+ * arrow functions).
+ *
+ * For careful use in rare cases, Immer's `castDraft` is provided as the second
+ * editor argument. It can cast parts of previous `Immutable` states to be
+ * 'mutable' for assignment to the next `draft` state. If you use this feature,
+ * items can be added to the draft state, but no changes be made to them.
  *
  * See {@link https://immerjs.github.io/immer/ | Immer docs} for more detail on
  * the conventions for Immer `producers`.
