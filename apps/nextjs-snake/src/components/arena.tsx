f91b8e71473e65c24ec4bde3cbab5025d56e323a
Cefn Hoile <github.com@cefn.com>
2021-06-29 00:31:28 +0100

    Scale to fit within viewport. Fix H1 margin bug. (#125)

--- apps/nextjs-snake/src/components/arena.tsx
+++ apps/nextjs-snake/src/components/arena.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { GRID_SPAN, StoreProps } from "../domain";
-import { SPRITE_SIDE } from "./graphics";
+import { SPRITE_SIDE, SCALE } from "./graphics";
 import { Fruit } from "./fruit";
 import { Score } from "./score";
 import { Snake } from "./snake";
@@ -10,9 +10,9 @@ export function Arena({ gameStore }: StoreProps) {
     <div
       style={{
         display: "block",
-        width: GRID_SPAN * SPRITE_SIDE,
-        height: GRID_SPAN * SPRITE_SIDE,
-        backgroundColor: "yellow",
+        width: GRID_SPAN * SPRITE_SIDE * SCALE,
+        height: GRID_SPAN * SPRITE_SIDE * SCALE,
+        backgroundColor: "yellow"
       }}
     >
       <Score {...{ gameStore }} />
